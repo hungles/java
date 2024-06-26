250c081ffab585956228598190dd5acb3d4f7fd3
Sebastian Carmona <scarmona04@hotmail.com>
2024-06-26 09:53:23 -0500

    while

---
+++ primera-app/src/bulcle_while.java
@@ -0,0 +1,12 @@
+public class bulcle_while {
+    public static void main(String[] args) throws Exception {
+    
+        int contador = 1;
+        
+        while(contador <= 5){
+            System.out.println(contador);
+            contador++;
+        }
+
+    }
+}
