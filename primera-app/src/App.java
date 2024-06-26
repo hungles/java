6df513ac4c7b87d9927be917eb4557ba2cc6c623
Sebastian Carmona <scarmona04@hotmail.com>
2024-06-25 20:31:02 -0500

    Añadiendo operadores

---
+++ primera-app/src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // Operadores: Simbolos que siven para hacer operaciones con variables o valores
+
+        //Aritmeticos:
+
+        int a = 1421;
+
+        int b = a % 2; // El resto nos sirve para saber si un numero es par o impar
+
+
+
+        System.out.println(b);
+
+        // ASIGNACION
+
+        int c = 5;
+        c=10;
+        c=21;
+        c+=15;
+        c-=30;
+        c*=2;
+        c/=2;
+        c++;
+        c++;
+        c--;
+        System.out.println(c);
+
+        
+    }
+}
