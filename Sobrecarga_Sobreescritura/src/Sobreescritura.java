c7fe0e26188eb04b62f8d2fadba4e1bcaa386d4f
Sebastian Carmona <scarmona04@hotmail.com>
2024-07-03 21:03:48 -0500

    Sobreescritura y sobrecarga

---
+++ Sobrecarga_Sobreescritura/src/Sobreescritura.java
@@ -0,0 +1,10 @@
+package Sobrecarga_Sobreescritura.src;
+
+public abstract class Sobreescritura {
+    
+    private int id_animal;
+    private String estado;
+
+    public abstract void hacerSonido();
+
+}
---
+++ Sobrecarga_Sobreescritura/src/Sobreescritura1.java
@@ -0,0 +1,8 @@
+package Sobrecarga_Sobreescritura.src;
+
+public class Sobreescritura1 extends Sobreescritura {
+    @Override
+    public void hacerSonido(){
+        System.out.println("Soy un gato y hago miau");
+    }
+}
