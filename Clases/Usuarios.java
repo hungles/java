4db7316b282bb310264edc7bcc61bfb70fc35798
scarmona <scarmona04@hotmail.com>
2024-10-07 18:43:01 -0500

    Añadiendo arhivo main y carpeta Clases

---
+++ Clases/Usuarios.java
@@ -0,0 +1,20 @@
+package Clases;
+public class Usuarios {
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String correo;
+
+
+    public Usuarios(String nombre, String apellido, int edad, String correo){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+    }
+
+    
+
+
+}
